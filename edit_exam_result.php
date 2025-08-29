ac208538e2e8c22e254e87b9030d7fc4d1e8b5c7
noppharut <noppharut.su@ku.th>
2025-08-29 17:55:31 +0700

    Only 1 StudentName In EditExam Get Bugged To Finished Work

--- edit_exam_result.php
+++ edit_exam_result.php
@@ -1,18 +1,18 @@
 <?php
 require("connect_db.php");
-$course_code = $_GET["course_code"];
-$sql = "SELECT * FROM exam_results WHERE course_code='$course_code'";
+$student_code = $_GET["student_code"];
+$sql = "SELECT * FROM exam_results WHERE student_code='$student_code'";
 $result = mysqli_query($conn, $sql);
-$course = mysqli_fetch_assoc($result);
+$student = mysqli_fetch_assoc($result);
 echo "<center>";
 echo "<form action=save_exam_result.php method=post>";
 echo "<table border=1 width=40%>";
-echo "<input type=hidden name=course_code_edit value=$course_code />";
-echo "<tr><td>Student Code:</td><td><input type=text name=course_code value=".$course["course_code"]."
+echo "<input type=hidden name=student_code_edit value=$student_code />";
+echo "<tr><td>Student Code:</td><td><input type=text name=student_code value=".$student["student_code"]."
 /></td></tr>";
-echo "<tr><td>Student Name:</td><td><input type=text name=student_code value=".$course["student_code"]."
+echo "<tr><td>Student Name:</td><td><input type=text name=student_name value=".$student["student_name"]."
 /></td></tr>";
-echo "<tr><td>Point:</td><td><input type=text name=point value=".$course["point"]." /></td></tr>";
+echo "<tr><td>Point:</td><td><input type=text name=point value=".$student["point"]." /></td></tr>";
 echo "<tr><td colspan=2><center><input type=submit value=Submit /></center></td></tr>";
 echo "</table>";
 echo "</form>";
--- edit_student.php
+++ edit_student.php
@@ -8,11 +8,11 @@ echo "<center>";
 echo "<form action=save_student.php method=post>";
 echo "<table border=1 width=40%>";
 echo "<input type=hidden name=student_code_edit value=$student_code />";
-echo "<tr><td>Student Code:</td><td><input type=text name=student_code value=".$student["student_code"]."
+echo "<tr><td>Code:</td><td><input type=text name=student_code value=".$student["student_code"]."
 /></td></tr>";
-echo "<tr><td>Student Name:</td><td><input type=text name=student_name value=".$student["student_name"]."
+echo "<tr><td>Name:</td><td><input type=text name=student_name value=".$student["student_name"]."
 /></td></tr>";
-echo "<tr><td>gender:</td><td><input type=text name=gender value=".$student["gender"]." /></td></tr>";
+echo "<tr><td>Gender:</td><td><input type=text name=gender value=".$student["gender"]." /></td></tr>";
 echo "<tr><td colspan=2><center><input type=submit value=Submit /></center></td></tr>";
 echo "</table>";
 echo "</form>";
