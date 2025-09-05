5d4dea2f330160dbf964b8ba77e27518a69c78e4
noppharut <noppharut.su@ku.th>
2025-09-05 14:14:30 +0700

    B-2

--- save_exam_result.php
+++ save_exam_result.php
@@ -4,7 +4,7 @@ $student_code_edit = $_POST["student_code_edit"];
 $student_code = $_POST["student_code"];
 $student_name = $_POST["student_name"];
 $point = $_POST["point"];
-$sql ="UPDATE students SET student_code='$student_code', student_name='$student_name',
+$sql ="UPDATE exam_results SET student_code='$student_code', student_name='$student_name',
 Point=$point WHERE student_code='$student_code_edit'";
 mysqli_query($conn, $sql);
 header("location: show_exam_result.php");
